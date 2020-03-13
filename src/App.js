5fab8221d6f7bec89bbeb90a7e36886d3aa00799
Mathieu Anderson <anderson@talon.one>
2020-03-13 10:41:30 +0100

    Eject cra, add theming for antd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Select } from 'antd'
+import './app.less'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Select defaultValue='lucy'>
+          <Select.Option value='jack'>jack</Select.Option>
+          <Select.Option value='lucy'>lucy</Select.Option>
+          <Select.Option value='disabled' disabled>
+            disabled
+          </Select.Option>
+          <Select.Option value='yiminghe'>yiminghe</Select.Option>
+        </Select>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
